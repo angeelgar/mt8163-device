6ea43a27388242fa159ebec8fdc36ba8d77893fd
张丛 <zhangcong@yongyida.com>
2017-12-08 09:38:06 +0800

    去掉主干上的系统服务编译配置

--- mediatek/common/device.mk
+++ mediatek/common/device.mk
@@ -2339,6 +2339,4 @@ ifeq ($(strip $(YYD_FACE_RECOGNITION_SUPPORT)), yes)
 	#PRODUCT_PACKAGES += YYDRobotFoundPeople
 	#PRODUCT_PACKAGES += opencv
 endif
-
-
-PRODUCT_PACKAGES += SystemService    #系统服务采用工程编译方式		
+	
